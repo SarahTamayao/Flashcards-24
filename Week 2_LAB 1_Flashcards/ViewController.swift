5ffe3a028bc80711db43254c5f5fcf37e18e5bba
Truong Dang <ddtblock1903@gmail.com>
2022-02-26 14:05:31 -0500

    Initial Commit

---
+++ Week 2_LAB 1_Flashcards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week 2_LAB 1_Flashcards
+//
+//  Created by Đặng Đức Trường on 2/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Week 2_LAB 1_FlashcardsUITests/Week_2_LAB_1_FlashcardsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Week_2_LAB_1_FlashcardsUITestsLaunchTests.swift
+//  Week 2_LAB 1_FlashcardsUITests
+//
+//  Created by Đặng Đức Trường on 2/26/22.
+//
+
+import XCTest
+
+class Week_2_LAB_1_FlashcardsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
